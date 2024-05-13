23af4219a7602aab606514ee13432d62a02ddc40
fanendrashelki <fanendrashelki@gmail.com>
2024-05-13 10:18:19 +0530

    change

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
-import cors from "cors";
 import dotenv from "dotenv";
-import bookRouter from "./routes/bookRouter.js";
+import cors from "cors";
+import route from "./routes/bookRouter.js";
 
 const app = express();
-
-// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 dotenv.config();
 
-// MongoDB Atlas connection
-const PORT = process.env.PORT || 8000;
-const MONGO_URL = process.env.MONGOURL;
+const PORT = process.env.PORT || 8000
+const URL = process.env.MONGOURL;
 
-mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("Connected to MongoDB Atlas");
-    app.listen(PORT, () => {
-      console.log(`Server is running on port: ${PORT}`);
-    });
+mongoose.connect(URL).then(()=>{
+  console.log("DB connected successfully");
+  app.listen(PORT, ()=>{
+    console.log(`server is running on port : ${PORT}`);
   })
-  .catch(error => {
-    console.error("Error connecting to MongoDB Atlas:", error);
-  });
 
-// Routes
-app.use('/api', bookRouter);
+}).catch(error => console.log(error)); 
 
-export default app;
+app.use('/api', route)
